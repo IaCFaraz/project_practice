a1618d84ac247e1b0fee999db7e818bbe48bab05
M Faraz <farazbashir1122@gmail.com>
2025-09-10 05:11:27 -0700

    DateDiff

---
+++ project_practice/DateDiff.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_practice
+{
+    internal class DateDiff
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt1 = new DateTime(2023, 6, 1);
+            DateTime dt2 = new DateTime(2024, 6, 1);
+            TimeSpan ts = dt2 - dt1;
+            Console.WriteLine("Difference in days: " + ts.TotalDays);
+            Console.WriteLine("Difference in hours: " + ts.TotalHours);
+            Console.WriteLine("Difference in minutes: " + ts.TotalMinutes);
+            Console.WriteLine("Difference in seconds: " + ts.TotalSeconds);
+        }
+    }
+}
