c519fa54fa8741cc783f0f40d7739f08435ee6f3
M Faraz <farazbashir1122@gmail.com>
2025-09-10 06:59:11 -0700

    operatorDemo

---
+++ project_practice/operatorDemo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_practice
+{
+    internal class OperatorDemo
+
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 30;
+            string result = (a > b) ? "a is greater" : "b is greater";
+            Console.WriteLine(result);
+
+        }
+
+        //{
+        //    static void Main(string[] args)
+        //    {
+        //        int a = 10;
+        //        int b = a++;   a++;  a++;
+        //        Console.WriteLine("a={0} and b={1}" , a++ ,++b);
+
+
+        //    }
+        //}
+    }
+}   
