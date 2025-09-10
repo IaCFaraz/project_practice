2e3dce68d11319863a6d892a766a02748b03dc6c
M Faraz <farazbashir1122@gmail.com>
2025-09-10 12:09:09 -0700

    Check

---
+++ project_practice/Check.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace project_practice
+{
+    internal class Check
+    {
+        static void Main(string[] args)
+        {
+            int num = 100;
+            string result = "ODD";
+
+            if (num % 2 == 0)
+            {
+                result = "EVEN";
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
