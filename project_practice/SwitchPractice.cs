3a35758f570b37b2268de938dd2132288a64ba82
M Faraz <farazbashir1122@gmail.com>
2025-09-13 11:39:29 -0700

    SwitchPractice

---
+++ project_practice/SwitchPractice.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_practice
+{
+    internal class SwitchPractice
+    {
+        static void Main(string[] args)
+        {
+            int num = 5;
+            switch (num % 2)
+            {
+                case 0 :
+                    Console.WriteLine("Even");
+                    break;
+                default:
+                    Console.WriteLine("add");
+                    break;
+
+            }
+
+        }
+
+    }
+}
